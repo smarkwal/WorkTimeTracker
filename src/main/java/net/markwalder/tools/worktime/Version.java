bce4d8d8e5a81aed0299a0cd78963da19aa0bcff
Stephan Markwalder <stephan@markwalder.net>
2016-09-18 22:40:31 +0200

    * Version changed to 2.0.1-SNAPSHOT. * Write version number to version.properties at build time. * Read version number from version.properties at runtime.

--- src/main/java/net/markwalder/tools/worktime/Version.java
+++ src/main/java/net/markwalder/tools/worktime/Version.java
@@ -1,11 +1,44 @@
 package net.markwalder.tools.worktime;
 
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Version {
 
-	private static final String version = "2.0";
+	public static final String UNKNOWN = "[unknown]";
 
+	/**
+	 * Get the current version number.
+	 *
+	 * @return Version number
+	 */
 	public static String getVersion() {
-		return version;
+		Properties properties = loadProperties();
+		return properties.getProperty("version", UNKNOWN);
+	}
+
+	/**
+	 * Load properties from version.properties on classpath.
+	 *
+	 * @return Properties
+	 */
+	private static Properties loadProperties() {
+		Properties properties = new Properties();
+		InputStream stream = null;
+		try {
+			stream = Version.class.getClassLoader().getResourceAsStream("version.properties");
+			if (stream != null) {
+				properties.load(stream);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			IOUtils.closeQuietly(stream);
+		}
+		return properties;
 	}
 
 }
