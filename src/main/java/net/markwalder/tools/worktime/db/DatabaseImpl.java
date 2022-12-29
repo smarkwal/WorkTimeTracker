8ee6fc7bcfa0b70cb0ce38866461a0d3979a07c5
smarkwal <stephan@markwalder.net>
2022-12-29 15:40:00 +0100

    Add license information, copyright headers, and reformat all source code.

--- src/main/java/net/markwalder/tools/worktime/db/DatabaseImpl.java
+++ src/main/java/net/markwalder/tools/worktime/db/DatabaseImpl.java
@@ -1,13 +1,28 @@
+/*
+ * Copyright 2022 Stephan Markwalder
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.markwalder.tools.worktime.db;
 
 import com.google.inject.Inject;
-import net.markwalder.tools.worktime.db.store.Store;
-import net.markwalder.tools.worktime.utils.DateTimeUtils;
-import org.apache.commons.lang3.time.DateUtils;
-
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import net.markwalder.tools.worktime.db.store.Store;
+import net.markwalder.tools.worktime.utils.DateTimeUtils;
+import org.apache.commons.lang3.time.DateUtils;
 
 public class DatabaseImpl implements Database {
 
@@ -159,7 +174,7 @@ public class DatabaseImpl implements Database {
 		int offset = DatabaseUtils.slot(startOfDayTime - startOfYearTime);
 		int length = DatabaseUtils.slot(endOfDayTime - startOfDayTime);
 
-		return new int[]{offset, length};
+		return new int[] { offset, length };
 
 	}
 
