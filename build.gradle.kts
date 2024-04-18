1b80ffd812190b4eb48f79a23f7776dde7294665
smarkwal <stephan@markwalder.net>
2024-04-18 22:00:41 +0200

    Bump transitive build dependencies to fix security vulnerabilities.

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,15 @@ plugins {
     id("org.jarhc") version "1.0.1"
 }
 
+buildscript {
+    dependencies {
+        // fix CVE-2023-3635 in Okio < 3.4.0
+        // (indirect dependency of Gradle Versions Plugin 0.51.0)
+        classpath("com.squareup.okio:okio:3.9.0")
+        classpath("com.squareup.okio:okio-jvm:3.9.0")
+    }
+}
+
 repositories {
     mavenCentral()
 }
